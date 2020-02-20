08ac626afd8a361060ea433d18cbd89e03ab2603
Тарас Срібняк <56640754+Luxiv@users.noreply.github.com>
2020-02-20 21:01:53 +0200

    Add urls

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,20 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import IndexView, StudentGroupsView, StudentGroupView, TachersListView, TeacherView
+from .views import IndexView, StudentGroupsView, StudentGroupView, TachersListView, TeacherView, WeekDayView
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
 
-    path('student/groups/', StudentGroupsView.as_view(), name='student_groups'),
-    path('student/groups/<str:name>/', StudentGroupView.as_view(), name='student_group'),
+    path('groups/', StudentGroupsView.as_view(), name='student_groups'),
+    path('groups/<str:name>/', StudentGroupView.as_view(), name='student_group'),
+    path('groups/day/<str:name>/', WeekDayView.as_view(), name='day_of_week'),
 
-    path('teacher/names/', TachersListView.as_view(), name='teachers_list'),
-    path('teacher/name/<str:name>/', TeacherView.as_view(), name='teacher'),
+    path('teachers/', TachersListView.as_view(), name='teachers_list'),
+    path('teachers/<str:name>/', TeacherView.as_view(), name='teacher'),
+
+
+
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import Lesson, Class, Teacher
+from .models import Lesson, Class, Teacher, Day
 from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
-
-
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+'''
+0867 44 6:43-ЛІКАРНЯ 7:20 ДУДАЄВА
+2131 26
+'''
 
 class StudentGroupsView(TemplateView):
     template_name = 'groups.html'
@@ -45,5 +47,15 @@ class TeacherView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['teacher'] = get_object_or_404(Teacher, name=self.kwargs['name'])
         lessons = Lesson.objects.filter(teacher=context['teacher'])
+        context['lessons'] = lessons
+        return context
+
+class WeekDayView(TemplateView):
+    template_name = 'week_day.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['day_of_week'] = get_object_or_404(Day, name=self.kwargs['name'])
+        lessons = Lesson.objects.filter(day_of_week=context['day_of_week'])
         context['lessons'] = lessons
         return context
